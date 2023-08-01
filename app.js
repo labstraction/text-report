19ad8f6d856e69a681bacd5a0ca93ed55f1fcc2d
labstraction <45163263+labstraction@users.noreply.github.com>
2023-08-01 09:43:56 +0200

    commit-b

--- app.js
+++ app.js
@@ -2,6 +2,24 @@ const fs = require("fs");
 
 const fileUrl = process.argv[2];
 
+// console.log(process.argv[2]);
+const outputUrl = createOutputUrl(fileUrl);
+
+
+
+function createOutputUrl(url){
+
+    // ./demo-files/pippo.txt  => ./demo-files/pippo-report.txt
+    
+    const splittedUrl = url.split('.');
+    // const finalUrl='.'+splittedUrl[1]+'-report.'+splittedUrl[2];
+    const lastPart = splittedUrl.pop();
+    const firstPart = splittedUrl.join('.');
+    const finalUrl = firstPart + "-report." +lastPart ;
+    return finalUrl;
+
+}
+
 
 
 
